6d1aea67edd6c5097d681e83e22d79202c8bcc98
harshmittal1750 <harshmittal1750@gmail.com>
2021-02-21 21:03:06 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {" "}
+      <Projects></Projects>
+      <Header></Header>
+      <Footer></Footer>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class footer extends Component {
+  render() {
+    return <div>footer components</div>;
+  }
+}
+
+export default footer;
